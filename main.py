d767504e095ecfe5b830c7a0630559de8d562ef7
Santiago Piñero <89141124+s-anti@users.noreply.github.com>
2023-08-09 22:50:25 -0300

    Estructura incial para empezar a trabajar

--- main.py
+++ main.py
@@ -1,83 +1,31 @@
-import sqlite3 as sql
+from db import Db
+from menu import Menu
 
+a = Db()
+estructura = {
+    "animal": {
+        "o seguimiento": lambda: print("Cargar el seguimiento de la vaca"),
+        "o informe": lambda: print("Ver seguimientos previos de la vaca"),
+        "o consultas": lambda: print("Ver información de todas las vacas"),
+    },
+    "campos": {
+        "consultas": {
+            "cambio parcelas": lambda: print("Cambio de parcelas"),
+            "cambio potrero": lambda: print("Cambio de potrero"),
+            "cambio campo": lambda: print("Cambio de campo"),
+        },
+        "listado potreros": lambda: print("Listado de potreros"),
+        "carga de potreros": lambda: print("Carga de potreros"),
+        "modificacion de potreros": lambda: print("Modificación de potreros"),
+        "baja de potreros": lambda: print("Baja de potreros"),
+    },
+    "usuario": {
+        "listado usuarios": lambda: print("Listado de usuarios"),
+        "carga de usuarios": lambda: print("Carga de usuarios"),
+        "modificacion de usuarios": lambda: print("Modificación de usuarios"),
+        "baja de usuarios": lambda: print("Baja de usuarios"),
+    },
+}
 
-class Main:
-    def __init__(self) -> None:
-        self.conn = sql.connect("base.sqlite3")
-        self.cur = self.conn.cursor()
-
-        self.cur.execute(
-            """CREATE TABLE IF NOT EXISTS cliente (
-                    id_cli INTEGER PRIMARY KEY AUTOINCREMENT,
-                    nombre VARCHAR2 NOT NULL,
-                    apellido VARCHAR2 NOT NULL,
-                    telefono VARCHAR2,
-                    email VARCHAR2 NOT NULL
-            )"""
-        )
-
-        self.cur.execute(
-            """CREATE TABLE IF NOT EXISTS animal (
-                id_anim INTEGER PRIMARY KEY AUTOINCREMENT,
-                id_padre INTEGER,
-                id_madre INTEGER NOT NULL,
-                fec_nac DATE NOT NULL,
-                peso_nac FLOAT NOT NULL,
-                hembra BOOLEAN NOT NULL,
-                cat INTEGER NOT NULL,
-                sub_cat INTEGER NOT NULL,
-                FOREIGN KEY (id_padre) REFERENCES animal (id),
-                FOREIGN KEY (id_madre) REFERENCES animal (id)
-            )
-            """
-        )
-
-        self.cur.execute(
-            """CREATE TABLE IF NOT EXISTS campo (
-                id_camp INTEGER PRIMARY KEY AUTOINCREMENT,
-                fec_alta DATE NOT NULL,
-                tipo_campo INTEGER NOT NULL,
-                nombre VARCHAR2 NOT NULL,
-                propietario VARCHAR2 NOT NULL,
-                telefono VARCHAR2 NOT NULL,
-                main VARCHAR2 NOT NULL,
-                hectareas FLOAT NOT NULL
-            )"""
-        )
-
-        self.cur.execute(
-            """CREATE TABLE IF NOT EXISTS potrero (
-                id_pot INTEGER PRIMARY KEY AUTOINCREMENT,
-                id_camp INTEGER NOT NULL,
-                ancho FLOAT NOT NULL CHECK(ancho > 0),
-                largo FLOAT NOT NULL CHECK(largo > 0),
-                car_animal FLOAT NOT NULL,
-                vol_pasto_n FLOAT NOT NULL,
-                vol_pasto_l FLOAT NOT NULL,
-                FOREIGN KEY (id_camp) REFERENCES campo (id)
-            )"""
-        )
-        # PASTO NATURAL E IMPLANTADO n y l
-
-        self.cur.execute(
-            """CREATE TABLE IF NOT EXISTS parcela (
-                id_parc INTEGER PRIMARY KEY AUTOINCREMENT,
-                id_pot INTEGER NOT NULL,
-                observaciones TEXT NULL,
-                FOREIGN KEY (id_pot) REFERENCES potrero (id)
-
-            )"""
-        )
-
-        self.cur.execute(
-            """CREATE TABLE IF NOT EXISTS seguimientos (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                id_anim INTEGER NOT NULL,
-                estado_desc TEXT NOT NULL,
-                fec_estimada DATE NOT NULL,
-                FOREIGN KEY (id_anim) REFERENCES animal (id)
-            )"""
-        )
-
-
-a = Main()
+menu = Menu(estructura)
+menu.iniciar()
---
+++ menu.py
@@ -0,0 +1,106 @@
+class Menu:
+
+    """Una clase para hacer menúes que sean
+    adaptables en base a diccionarios"""
+
+    posicion = ""
+    # Que tan "profundo" nos encontramos en el menú
+
+    def __init__(self, dicc: dict, linea: str = "______________________"):
+        # linea es el separador que vamos a usar
+        self.linea = linea
+
+        # dicc es el diccionario principal de la clase
+        self.dicc = dicc
+
+    def validar(self, numeros: tuple):
+        # Función de validación, va desde numeros[0] a numeros[-1]
+        # pero excluye todos los del medio
+        while True:
+            v = input(f"Ingrese un valor ({numeros[0]} - {numeros[-1]}): ")
+
+            if not v.isnumeric():
+                print("Debe ingresar un numero...\n")
+                continue
+
+            v = int(v)
+
+            if not numeros[0] <= v <= numeros[1]:
+                print(f"Debe ser entre {numeros[0]} - {numeros[-1]}...\n")
+                continue
+            return v
+
+    def recursiva(self, diccionario, llave):
+        while True:
+            print(llave)  # Titulo principal
+            print(self.linea)
+
+            numero = 0
+            for opcion in diccionario[llave]:
+                numero += 1
+                print(f"{numero}: {opcion}")
+
+            print(self.linea)
+
+            print("0: Salir")
+
+            print("\n" * 1)
+
+            # numero es la ultima opcion
+            opcion = self.validar((0, numero))
+            print("\n" * 1)
+
+            if opcion == 0:
+                break
+
+            # key de la opcion seleccionada, ej: "Submenu propietarios"
+            nombre_opcion = list(diccionario[llave].keys())[opcion - 1]
+
+            # Valor de esta opcion seleccionada
+            item_menu = diccionario[llave][nombre_opcion]
+
+            # Si la opcion es otro diccionario
+            if isinstance(item_menu, dict):
+                self.recursiva(diccionario[llave], nombre_opcion)
+
+            # Si no lo es, llamamos la función que contiene
+            elif callable(item_menu):
+                item_menu()
+                print("\n")
+
+            # Si todo salió bien, repetimos el bucle
+
+    def iniciar(self):
+        self.recursiva(self.dicc, [*self.dicc.keys()][0])
+
+
+if __name__ == "__main__":
+    # Solo se ejecuta si ejecutamos este código, no si lo importamos en otro lado
+    # Para revisar bugs más rápido
+    mi_menu = {
+        "Menu principal": {
+            "Menu Ingresar": {
+                "Ingresar Proveedores": lambda: print("Ingresar Proveedores"),
+                "Ingresar Productos": lambda: print("Ingresar Productos"),
+            },
+            "Menu Leer": {
+                "Menu secreto misterioso": {
+                    "Opcion 1": lambda: print("Opcion 1"),
+                    "Opcion 2": lambda: print("Opcion 2"),
+                },
+                "Leer Proveedores": lambda: print("Leer Proveedores"),
+                "Leer Productos": lambda: print("Leer Productos"),
+            },
+            "Menu Actualizar": {
+                "Actualizar Proveedores": lambda: print("Actualizar Proveedores"),
+                "Actualizar Productos": lambda: print("Actualizar Productos"),
+            },
+            "Menu Eliminar": {
+                "Eliminar Proveedores": lambda: print("Eliminar Proveedores"),
+                "Eliminar Productos": lambda: print("Eliminar Productos"),
+            },
+        }
+    }
+
+    menu = Menu(mi_menu)
+    menu.iniciar()
